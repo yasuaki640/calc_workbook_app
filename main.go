3950e8bdf5b72a6ef6b5092ca56b8fc6caa15afa
yasuaki640 <yasuaki640@gmail.com>
2020-09-14 21:48:17 +0900

    send map to tmpl

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "こんにちは Gin",
+		})
 	})
 	router.Run(":8000")
 }
