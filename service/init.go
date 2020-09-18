64d9adcfbe13e1cbc5af64180251ef58e0f4fe80
yasuaki640 <yasuaki640@gmail.com>
2020-09-18 21:08:09 +0900

    define func of connect db

--- service/init.go
+++ service/init.go
@@ -1,1 +1,14 @@
 package service
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func init() {
+	dsn := "sqlite.db"
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database.")
+	}
+}
