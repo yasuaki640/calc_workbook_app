9e123aba4e2a06c230b104896543ff227428fdae
yasuaki640 <yasuaki640@gmail.com>
2020-09-27 18:26:31 +0900

    dont call db conn initializer in main func

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/yasuaki640/go-crud/controller"
-	"github.com/yasuaki640/go-crud/service"
 )
 
 func main() {
-	service.Init()
 	router := gin.Default()
 
 	v1 := router.Group("/human/v1")
