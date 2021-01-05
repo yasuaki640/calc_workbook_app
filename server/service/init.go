e07fd938b1d090cb926b5d1a2695e469ce976b36
yasuaki640 <yasuaki640@gmail.com>
2021-01-05 22:43:23 +0900

    .envファイルでDBへの接続情報を管理する

--- server/service/init.go
+++ server/service/init.go
@@ -1,22 +1,31 @@
 package service
 
 import (
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func InitDB() *gorm.DB {
-	dsn := "host=db " +
-		"user=postgres " +
-		"password=postgres " +
-		"dbname=go-crud " +
-		"port=5432 " +
-		"sslmode=disable " +
-		"TimeZone=Asia/Tokyo"
+
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	dsn := "host=" + os.Getenv("DB_HOST") + " " +
+		"user=" + os.Getenv("DB_USER") + " " +
+		"password=" + os.Getenv("DB_PASSWORD") + " " +
+		"dbname=" + os.Getenv("DB_NAME") + " " +
+		"port=" + os.Getenv("DB_PORT") + " " +
+		"sslmode=" + os.Getenv("DB_SSL_MODE") + " " +
+		"TimeZone=" + os.Getenv("DB_TIME_ZONE")
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
+
 	return db
 }
