17ea6a9fc97668d993beefecad1ea2dc2a18f2ec
yasuaki640 <yasuaki640@gmail.com>
2021-01-09 12:58:10 +0900

    alter sql dialect for create table  to postgresql

--- server/migrations/2_create_humans_table.up.sql
+++ server/migrations/2_create_humans_table.up.sql
@@ -1,10 +1,13 @@
 CREATE TABLE humans
 (
-    id         integer primary key,
-    name       text,
-    sex        integer,
-    status     text,
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime
+    id           serial
+        constraint humans_pk primary key,
+    name         VARCHAR(256) DEFAULT 'Anonymous' NOT NULL,
+    sex          SMALLINT NOT NULL,
+    favorability SMALLINT     DEFAULT 0 NOT NULL,
+    description  TEXT,
+    created_at   DATE     NOT NULL,
+    update_at    DATE,
+    deleted_at   DATE
 );
+
