79e47f7681ada0717785bdd48eafc5f771d16d7b
yasuaki640 <yasuaki640@gmail.com>
2021-01-09 21:21:15 +0900

    add new columns to model

--- server/model/human.go
+++ server/model/human.go
@@ -2,13 +2,16 @@ package model
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Human struct {
 	gorm.Model
-	Name   string `json:"name" validate:"required"`
-	Sex    byte   `json:"sex" validate:"required"`
-	Status string `json:"status" validate:"required"`
+	Name         string    `json:"name" validate:"required"`
+	Sex          byte      `json:"sex" validate:"required"`
+	Birthday     time.Time `json:"birthday"`
+	Description  string    `json:"description"`
+	Favorability byte      `json:"favorability" validate:"required,gte=0,lte=10"`
 }
 
 func (Human) TableName() string {
