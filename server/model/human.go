a2755c739241e6e58ae613802aa6b45a1d5eaa32
yasuaki640 <yasuaki640@gmail.com>
2021-01-09 21:40:44 +0900

    jsonのkey名もgormの規約に合わせちゃう

--- server/model/human.go
+++ server/model/human.go
@@ -7,11 +7,11 @@ import (
 
 type Human struct {
 	gorm.Model
-	Name         string    `json:"name" validate:"required"`
-	Sex          byte      `json:"sex" validate:"required"`
-	Birthday     time.Time `json:"birthday"`
-	Description  string    `json:"description"`
-	Favorability byte      `json:"favorability" validate:"required,gte=0,lte=10"`
+	Name         string `validate:"required"`
+	Sex          byte   `validate:"required"`
+	Birthday     time.Time
+	Description  string
+	Favorability byte `validate:"required,gte=0,lte=10"`
 }
 
 func (Human) TableName() string {
