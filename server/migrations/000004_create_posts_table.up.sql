8da2f321c861f2f7ff05b9105d73beef2966da25
yasuaki640 <yasuaki640@gmail.com>
2021-01-11 10:57:02 +0900

    add rows

--- server/migrations/000004_create_posts_table.up.sql
+++ server/migrations/000004_create_posts_table.up.sql
@@ -2,14 +2,17 @@ BEGIN;
 
 CREATE TABLE go_crud.posts
 (
-    id       SERIAL
+    id         SERIAL
         CONSTRAINT posts_pk
             PRIMARY KEY,
-    human_id INT
+    human_id   INT
         CONSTRAINT posts_humans_id_fk
             REFERENCES humans
             ON UPDATE CASCADE ON DELETE CASCADE,
-    caption  TEXT
+    caption    TEXT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 ALTER TABLE go_crud.posts
     OWNER TO go_crud;
